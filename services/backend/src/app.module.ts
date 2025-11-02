18b620d7a7cc4ed4ef107af31e56f24504e72d7e
sewaca <sewaca7@gmail.com>
2025-11-02 21:25:01 +0300

    фиксы + доделки по бекенду

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { HelloWorldModule } from "./features/HelloWorld/HelloWorld.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HelloWorldModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { FastifyAdapter } from "@nestjs/platform-fastify";
+
+const MAX_BODY_SIZE = 10 * 1024;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(
+    AppModule,
+    new FastifyAdapter({ bodyLimit: MAX_BODY_SIZE }),
+    { bufferLogs: true },
+  );
+  app.enableShutdownHooks();
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
