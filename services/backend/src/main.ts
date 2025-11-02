54f07646eafabbe63d1418ce675c8baefb064dbd
sewaca <sewaca7@gmail.com>
2025-11-02 21:26:47 +0300

    fixed

--- services/backend/src/main.ts
+++ services/backend/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import { FastifyAdapter } from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
 
 const MAX_BODY_SIZE = 10 * 1024;
 
