52a26f0138ef6631c7c90a02430d594ce33be7ef
sewaca <53119694+sewaca@users.noreply.github.com>
2025-11-05 01:33:53 +0300

    created pre-commit hook (#21)
    * fix???
    
    * move to self-written
    
    * fix
    
    * fixes

--- infra/check_pr_title.py
+++ infra/check_pr_title.py
@@ -6,21 +6,19 @@ import os
 if len(sys.argv) < 2:
     print("FATAL: Did not get any pr title")
     sys.exit(1)
-
 pr_title = sys.argv[1]
-print("pr title is", pr_title)
+print("pr title is: ", pr_title)
 
 # Получаем теги из переменной окружения TAGS
 tags = os.getenv('TAGS')
 if not tags:
     print("FATAL: Did not get tags array")
     sys.exit(1)
-
 tag_list = [tag.strip() for tag in tags.split('\n')]
+print("available tags are: ", tag_list)
 
-print("tag_list is ", tag_list)
-
-if any(pr_title.startswith(tag) for tag in tag_list): 
+# tag != '' is workarount for safe use
+if any(pr_title.startswith(tag) and tag != '' for tag in tag_list): 
     sys.exit(0)
 
 print(f"Wrong title format. None of correct tags found in {pr_title}")
