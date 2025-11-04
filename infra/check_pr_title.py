be26beb0f5b8c89cc2f4b4e8beffcce44d7600dd
sewaca <sewaca7@gmail.com>
2025-11-05 00:56:47 +0300

    move to self-written

---
+++ infra/check_pr_title.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+def main():
+    # Получаем заголовок PR из аргументов командной строки
+    if len(sys.argv) < 2:
+        print("FATAL: Did not get any pr title")
+        sys.exit(1)
+
+    pr_title = sys.argv[1]
+
+    # Получаем теги из переменной окружения TAGS
+    tags = os.getenv('TAGS')
+    if not tags:
+        print("FATAL: Did not get tags array")
+        sys.exit(1)
+
+    tag_list = [tag.strip() for tag in tags.split('\n')]
+    
+    if any(pr_title.startswith(tag) for tag in tag_list): 
+        sys.exit(0)
+
+    print(f"Wrong title format. None of correct tags found in {pr_title}")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
