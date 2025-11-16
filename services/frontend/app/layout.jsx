320bf70e868a6246eb6cba2f666ca068631b1890
sewaca <53119694+sewaca@users.noreply.github.com>
2025-11-16 20:58:59 +0300

    chore: some local improvements (#35)
    Co-authored-by: madara-robot <madara-robot@github.com>

---
+++ services/frontend/app/layout.jsx
@@ -0,0 +1,20 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+export const metadata = { title: 'Supreme-Infra application' };
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ru">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
