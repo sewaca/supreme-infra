dc6d86b47b9f53f245937eabd56923febceea68d
sewaca <sewaca7@gmail.com>
2025-11-28 02:37:24 +0300

    dd

--- services/frontend/jest.config.ts
+++ services/frontend/jest.config.ts
@@ -1,4 +1,15 @@
-import baseJestConfig from '../../jest.config.global';
+import { resolve } from 'node:path';
+import { cwd } from 'node:process';
+
+// Get absolute path to root directory
+// When running from services/frontend, go up two levels to reach root
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+const currentDir = (typeof __dirname !== 'undefined' ? __dirname : cwd());
+const rootDir = resolve(currentDir, '../..');
+const globalConfigPath = resolve(rootDir, 'jest.config.global.ts');
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const baseJestConfig = require(globalConfigPath);
 
 export default {
   ...baseJestConfig,
