6e993cfff020bd5aa5f93567269362b1ae0c2d47
sewaca <sewaca7@gmail.com>
2026-01-04 20:28:42 +0300

    fix: debug

--- services/frontend/instrumentation.ts
+++ services/frontend/instrumentation.ts
@@ -1,6 +1,10 @@
 // Instrumentation should only run in Node.js runtime, not Edge Runtime
 // Check Next.js runtime environment variable
 export async function register() {
+  console.log(
+    '[debug] inside register. NEXT_RUNTIME = ',
+    process.env.NEXT_RUNTIME,
+  );
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('./instrumentation.nodejs');
   }
