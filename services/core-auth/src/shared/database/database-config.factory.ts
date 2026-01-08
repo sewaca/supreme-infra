ff69dbb5d9affca9cd242f61187c0cf297e60dc7
sewaca <sewaca7@gmail.com>
2026-01-09 00:19:38 +0300

    minor: remove old services

--- services/core-auth/src/shared/database/database-config.factory.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { createDatabaseConfig as createBaseDatabaseConfig } from '@supreme-int/nestjs-shared';
-
-export function createDatabaseConfig(configService: ConfigService): TypeOrmModuleOptions {
-  return createBaseDatabaseConfig(configService, {
-    entities: [],
-  });
-}
--- services/core-auth/src/shared/decorators/roles.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import type { UserRole } from '../../features/Auth/model/Users.service';
-
-export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
