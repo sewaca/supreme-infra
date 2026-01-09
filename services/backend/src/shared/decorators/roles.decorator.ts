b555c5d42065880807bd89f0faba4ddbae2690cb
sewaca <sewaca7@gmail.com>
2026-01-09 13:06:10 +0300

    minor: remove empty backend service

--- services/backend/src/shared/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export type UserRole = 'user' | 'moderator' | 'admin';
-
-export const Roles = (...roles: UserRole[]) => SetMetadata('roles', roles);
