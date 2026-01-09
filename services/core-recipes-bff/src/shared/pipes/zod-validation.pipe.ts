5c8b66cd6459a69e94d55c717d4085b19c628aa7
sewaca <sewaca7@gmail.com>
2026-01-09 14:03:33 +0300

    fix: kdkd

--- services/core-recipes-bff/src/shared/pipes/zod-validation.pipe.ts
+++ services/core-recipes-bff/src/shared/pipes/zod-validation.pipe.ts
@@ -6,7 +6,9 @@ export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodType) {}
 
   transform(value: unknown) {
+    console.log('[test] value is: ', value);
     const result = this.schema.safeParse(value);
+    console.log(`[test] validation result.success=${result.success}`);
     if (!result.success) {
       const errors = result.error.issues.map((err) => ({ path: err.path.join('.'), message: err.message }));
       throw new BadRequestException({ message: 'Validation failed', errors });
