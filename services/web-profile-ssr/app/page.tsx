fd8f852f646fc7f2127d577ed01f74fc252df9f6
sewaca <sewaca7@gmail.com>
2026-01-10 21:27:11 +0300

    fix: gen

--- services/web-profile-ssr/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h1>web-profile-ssr</h1>
-      <p>profile </p>
-    </main>
-  );
-}
-
---
+++ services/web-profile-ssr/app/subjects/ranking/page.tsx
@@ -0,0 +1,9 @@
+const RangingPage = () => {
+    return (
+        <div>
+            <h1>Ranging</h1>
+        </div>
+    );
+};
+
+export default RangingPage;
