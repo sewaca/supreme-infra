6b6dec35ad2cb1e1db593c8c1c0c457cdc733ddd
sewaca <sewaca7@gmail.com>
2026-01-10 21:29:00 +0300

    fix: shit

--- services/web-profile-ssr/app/subjects/ranking/page.tsx
+++ services/web-profile-ssr/app/subjects/ranking/page.tsx
@@ -1,9 +1,9 @@
 const RangingPage = () => {
-    return (
-        <div>
-            <h1>Ranging</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Ranging</h1>
+    </div>
+  );
 };
 
 export default RangingPage;
