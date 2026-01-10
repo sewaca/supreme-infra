1bc04ab53ce41c80e0a984747f6a1bc7092f8ca0
sewaca <sewaca7@gmail.com>
2026-01-10 19:20:41 +0300

    major: new service

---
+++ services/web-documents-ssr/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>web-documents-ssr</h1>
+      <p>documents presentation </p>
+    </main>
+  );
+}
+
---
+++ services/web-documents-ssr/src/shared/lib/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  port: process.env.PORT || '3005',
+  nodeEnv: process.env.NODE_ENV || 'development',
+  isDevelopment: process.env.NODE_ENV === 'development',
+  isProduction: process.env.NODE_ENV === 'production',
+};
+
