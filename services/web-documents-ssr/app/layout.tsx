3f48489ce6420c14e8cb91fb325d8768222b2b03
sewaca <sewaca7@gmail.com>
2026-01-10 19:21:00 +0300

    fix: kk

--- services/web-documents-ssr/app/layout.tsx
+++ services/web-documents-ssr/app/layout.tsx
@@ -1,6 +1,6 @@
 import '@supreme-int/design-system/font.css';
 import '@supreme-int/design-system/theme.css';
-import { Header } from '@supreme-int/design-system/src/components/Header/Header';
+
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = { title: 'Supreme Infra' };
@@ -8,10 +8,7 @@ export const metadata: Metadata = { title: 'Supreme Infra' };
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ru">
-      <body>
-        <Header logoText="🍳 Taste.IT" logoHref="/" />
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
