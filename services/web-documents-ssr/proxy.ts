9ca82a00c4fd2ac92b6c1cd631c76db273fac76b
sewaca <sewaca7@gmail.com>
2026-01-10 21:21:33 +0300

    fix: tsc

--- services/web-documents-ssr/proxy.ts
+++ services/web-documents-ssr/proxy.ts
@@ -1,4 +1,3 @@
-import { createJwtAuthMiddleware } from '@supreme-int/nextjs-shared/src/shared/jwt/createJwtAuthMiddleware';
 import { chain } from '@supreme-int/nextjs-shared/src/shared/middleware/chain';
 
 export const proxy = chain([
