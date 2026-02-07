87cdc2cb73cc37865020db9362d6e93f2a0cd5e2
sewaca <sewaca7@gmail.com>
2026-02-07 03:35:29 +0300

    fix: code clarity

--- services/web-profile-ssr/app/subjects/ranking/actions.ts
+++ services/web-profile-ssr/app/subjects/ranking/actions.ts
@@ -2,19 +2,19 @@
 
 export const saveChoices = async (choices: { [key: string]: string | number }[][]): Promise<boolean> => {
   'use server';
-  console.log('[debug] saved choices are', choices);
+  console.log(`[debug] saved choices are: ${JSON.stringify(choices)}`);
 
   await new Promise((resolve) => setTimeout(resolve, 500));
 
   const isError = Math.random() > 0.7;
   if (isError) {
     // return status 500
-    console.log('[debug] failed to save choices');
+    console.log('[debug] random error occurred');
     throw new Error('Failed to save choices');
   }
 
   const result = Math.random() > 0.5;
 
-  console.log(`[debug] result is : ${result}`);
+  console.log(`[debug] status: "200", result: ${result}`);
   return result;
 };
