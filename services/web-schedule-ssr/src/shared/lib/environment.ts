bc35253df067c04faca315de6d295d8f7a0ef1d8
sewaca <sewaca7@gmail.com>
2026-03-29 20:08:46 +0300

    fix: overrides

--- services/web-schedule-ssr/src/shared/lib/environment.ts
+++ services/web-schedule-ssr/src/shared/lib/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   nodeEnv: process.env.NODE_ENV || 'development',
   isDevelopment: process.env.NODE_ENV === 'development',
   isProduction: process.env.NODE_ENV === 'production',
-  coreScheduleUrl: process.env.CORE_SCHEDULE_URL || 'http://localhost:8003/core-schedule',
-  coreClientInfoUrl: process.env.CORE_CLIENT_INFO_URL || 'http://localhost:8000/core-client-info',
+  coreScheduleUrl: process.env.CORE_SCHEDULE_URL || 'http://core-schedule.default.svc.cluster.local/core-schedule',
+  coreClientInfoUrl:
+    process.env.CORE_CLIENT_INFO_URL || 'http://core-client-info.default.svc.cluster.local/core-client-info',
 };
